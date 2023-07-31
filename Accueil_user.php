2133b22fe04681a18fcec818e286694b8c1d208e
aminatamrg <139908455+aminatamrg@users.noreply.github.com>
2023-07-31 21:36:35 +0000

    Create Accueil_user.php

---
+++ Accueil_user.php
@@ -0,0 +1,51 @@
+<!DOCTYPE HTML>
+<head>
+		<title>Accueil</title>
+		<meta charset="utf-8" />
+	    <link rel="stylesheet"  href="style_a.css">
+
+    <style>
+        /* Styles pour le header */
+	body {
+		margin: 0;
+        padding: 0;
+        align-items: center;
+        justify-content: center;
+        min-height: 100vh;
+        background-color: aquamarine;
+    }
+	.listimg {
+        display: flex;
+        width: 100%;
+        vertical-align: top;
+    }
+	header {
+		background-color: #4CAF50;
+        padding: 50px;
+        color: white;
+        text-align: center;
+}
+
+</style>
+</head>
+<body>
+	<header>
+		<h1>Page d'Accueil</h1>
+		<nav>
+			<ul>
+				<li><a href="gestion_patient.php">Accueil</a></li>
+				<li><a href="Fiche de rendez vous.php">Rendez-vous</a></li>
+				
+			</ul>
+		</nav>
+	</header>
+	<div class="listimg listimg_1_of_2">
+		<img src="1.PNG">
+		<img src="2.PNG">
+		<img src="3.JPG">
+	</div>
+	
+</body>
+</html>
+
+		   

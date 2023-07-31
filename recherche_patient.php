2eab723b824d0d539de5583bfda4d356e181362e
aminatamrg <139908455+aminatamrg@users.noreply.github.com>
2023-07-31 21:57:55 +0000

    Create recherche_patient.php

---
+++ recherche_patient.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Recherche des patients</title>
+    <link rel="stylesheet" href="style_1.css">
+  </head>
+  <body>
+    <section>
+    <h1>Recherche de patients</h1>
+    <form action="#" method="get">
+      <label for="nom">Email :</label>
+      <input type="text"  name="email" placeholder="email chercher" required>
+      <br>
+      <input type="submit" value="Rechercher">
+      
+    </form>
+</section>
+  </body>
+</html>

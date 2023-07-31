b55c63314ce03fbd45a47236d2618492f211dead
aminatamrg <139908455+aminatamrg@users.noreply.github.com>
2023-07-31 22:15:02 +0000

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,143 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+  <title>MedicoFILE | Contact</title>
+  <link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+  <link href='http://fonts.googleapis.com/css?family=Ropa+Sans' rel='stylesheet' type='text/css'>
+</head>
+<body>
+  <!--start-wrap-->
+
+  <!--start-header-->
+  <div class="header">
+    <div class="wrap">
+      <!--start-logo-->
+      <div class="logo">
+        <a href="index.html" style="font-size: 30px;">MedicoFILE</a>
+      </div>
+      <!--end-logo-->
+      <!--start-top-nav-->
+      <div class="top-nav">
+        <ul>
+          <li><a href="index.php">Accueil</a></li>
+          <li class="active"><a href="#">Contact</a></li>
+        </ul>
+      </div>
+      <div class="clear"> </div>
+      <!--end-top-nav-->
+    </div>
+    <!--end-header-->
+  </div>
+  <div class="clear"> </div>
+  <div class="wrap">
+    <div class="contact">
+      <div class="section group">
+        <div class="col span_1_of_3">
+          <div class="company_address">
+            <h2>Adresse Hôpital :</h2>
+            <p>Rue6x31, Medina, Dakar</p>
+            <p>Code postal 13000,</p>
+            <p>Sénégal</p>
+            <p>Téléphone : 77455586662</p>
+            <p>Fax : 3344689746654</p>
+            <p>Email : <span>MedicoFILE@gmail.com</span></p>
+          </div>
+        </div>
+        <div class="col span_2_of_3">
+          <div class="contact-form">
+            <h2>Ajouter un contact</h2>
+            <form action="config.php" method="post">
+              <div>
+                <span><label>Nom complet</label></span>
+                <span><input type="text" name="nom_complet" required></span>
+              </div>
+              <div>
+                <label for="telephone">Téléphone :</label>
+                <input type="tel" name="telephone" required><br>
+              </div>
+              <div>
+                <span><input type="submit" value="Ajouter"></span>
+              </div>
+            </form>
+          </div>
+        </div>
+        <div class="clear"> </div>
+      </div>
+      <div class="clear"> </div>
+    </div>
+  </div>
+ 
+</body>
+</html>
+
+
+<!DOCTYPE HTML>
+<html>
+<head>
+  <title>MedicoFILE | Contact</title>
+  <link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+  <link href='http://fonts.googleapis.com/css?family=Ropa+Sans' rel='stylesheet' type='text/css'>
+</head>
+<body>
+  <!--start-wrap-->
+
+  <!--start-header-->
+  <div class="header">
+    <div class="wrap">
+      <!--start-logo-->
+      <div class="logo">
+        <a href="index.html" style="font-size: 30px;">MedicoFILE</a>
+      </div>
+      <!--end-logo-->
+      <!--start-top-nav-->
+      <div class="top-nav">
+        <ul>
+          <li><a href="index.php">Accueil</a></li>
+          <li class="active"><a href="#">Contact</a></li>
+        </ul>
+      </div>
+      <div class="clear"> </div>
+      <!--end-top-nav-->
+    </div>
+    <!--end-header-->
+  </div>
+  <div class="clear"> </div>
+  <div class="wrap">
+    <div class="contact">
+      <div class="section group">
+        <div class="col span_1_of_3">
+          <div class="company_address">
+            <h2>Adresse Hôpital :</h2>
+            <p>Rue6x31, Medina, Dakar</p>
+            <p>Code postal 13000,</p>
+            <p>Sénégal</p>
+            <p>Téléphone : 77455586662</p>
+            <p>Fax : 3344689746654</p>
+            <p>Email : <span>MedicoFILE@gmail.com</span></p>
+          </div>
+        </div>
+        <div class="col span_2_of_3">
+          <div class="contact-form">
+            <h2>Ajouter un contact</h2>
+            <form action="traiter_ajout_contact.php" method="post">
+              <div>
+                <span><label>Nom complet</label></span>
+                <span><input type="text" name="nom_complet" required></span>
+              </div>
+              <div>
+                <label for="telephone">Téléphone :</label>
+                <input type="tel" name="telephone" required><br>
+              </div>
+              <div>
+                <span><input type="submit" value="Ajouter"></span>
+              </div>
+            </form>
+          </div>
+        </div>
+        <div class="clear"> </div>
+      </div>
+      <div class="clear"> </div>
+    </div>
+  </div> 
+</body>
+</html>
